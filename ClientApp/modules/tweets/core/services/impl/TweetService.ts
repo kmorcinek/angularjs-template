b854234b5e44e5b30e47d488e97ba63d8e9829d3
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2017-05-17 18:50:45 +0200

    Try to use jQuery globally
    and I don't know how

--- ClientApp/modules/tweets/core/services/impl/TweetService.ts
+++ ClientApp/modules/tweets/core/services/impl/TweetService.ts
@@ -1,10 +1,17 @@
 import {Tweet} from "../../entities/Tweet";
+import "jQuery"
+
 export class TweetService {
     public getAll(): Array<Tweet> {
         var returnObj: Array<Tweet> = new Array<Tweet>();
         for (var i: number = 0; i < 5; i++) {
             returnObj.push(new Tweet("@user", "Content", false));
         }
+
+        // Try also uncommenting the second
+        jQuery('body').css('color', 'green');
+        //$('body').css('color', 'red');
+
         return returnObj;
     }
 }
